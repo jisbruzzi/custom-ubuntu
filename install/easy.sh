d96bf762578d8617115994f51c1230c9303eb7aa
jisbruzzi <jose.sbru@gmail.com>
2020-07-11 12:09:55 -0300

    july 2020

--- install/easy.sh
+++ install/easy.sh
@@ -1,1 +1,1 @@
-sudo apt install i3 git vim xfe rofi gimp inkscape xbacklight stow blueman httpie feh udiskie
+sudo apt install i3 git vim xfe rofi gimp inkscape xbacklight stow blueman httpie feh udiskie alacritty
